ec7238ef5650eff498cb530c8f7cec8a89ee816a
Divyanshu Singh <rajpootdivyanshu37@gmail.com>
2023-03-25 15:46:46 +0530

    third  commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000
 const mongoDB = require("./db")
 mongoDB();
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://foodiehunger.netlify.app/");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
